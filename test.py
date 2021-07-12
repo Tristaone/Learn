e661a77a102d6335e01db506b86baf56ced740d0
Tristaone <554263281@qq.com>
2021-07-12 18:04:32 +0800

    learn first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/12 5:52 下午
+# @Author  : Tang！！
+# @ProjectName: Learn
+# @FileName: test.py
+# @Software: PyCharm
+
+bb = 'good'
---
+++ test_ignore.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/12 5:51 下午
+# @Author  : Tang！！
+# @ProjectName: Learn
+# @FileName: test_ignore.py
+# @Software: PyCharm
+
+aa = 'hi, everyone'
